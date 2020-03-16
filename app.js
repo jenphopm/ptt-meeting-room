bf41ca71e1d3b56b4b074ac6485b19de7e9325c6
jenphopm <jenphopm@gmail.com>
2020-03-16 23:13:29 +0700

    bot push2

--- app.js
+++ app.js
@@ -12,8 +12,9 @@ app.use(bodyParser.json())
 app.post('/webhook', (req, res) => {
     var text = req.body.events[0].message.text
     var sender = req.body.events[0].source.userId
+    var group = req.body.events[0].source.groupId
     var replyToken = req.body.events[0].replyToken
-    console.log(text, sender, replyToken)
+    console.log(text, sender, group, replyToken)
     console.log(typeof sender, typeof text)
     // console.log(req.body.events[0])
     if (text === 'สวัสดี' || text === 'Hello' || text === 'hello') {
